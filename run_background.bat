fca83c58a8090876dcfa483e562e9532ff94ebd0
Florian Wachter <florian.wachter698@gmail.com>
2026-01-08 11:18:50 +0100

    Update background run script

--- run_background.bat
+++ run_background.bat
@@ -1,3 +1,15 @@
 @echo off
-set MAX_INSCRIPTIONS=3
+:: Consolidated Background Run Script
+:: Usage: run_background.bat [count]
+:: Default count is 50 if no argument is provided.
+
+set "INSCRIPTIONS=%~1"
+if "%INSCRIPTIONS%"=="" set "INSCRIPTIONS=50"
+
+echo [AGKI] Starting background tagging for %INSCRIPTIONS% inscriptions...
+echo [AGKI] Logging output to data\logs\background_run.log
+
+set MAX_INSCRIPTIONS=%INSCRIPTIONS%
 python -m source.main >> data\logs\background_run.log 2>&1
+
+echo [AGKI] Process finished.
--- run_background_10.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set MAX_INSCRIPTIONS=10
-python -m source.main >> data\logs\background_run_10.log 2>&1
